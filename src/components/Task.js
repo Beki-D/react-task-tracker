174efd12afac684c96e4b1f7fd5b5092368eda02
beki-d <bk14drobs@gmail.com>
2022-10-31 13:39:38 +0300

    Assigned tooltip to each task, imported the css

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,19 +1,29 @@
 import { FaTimes } from "react-icons/fa"
+import { Tooltip } from "react-tippy"
+import 'react-tippy/dist/tippy.css'
 
 const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={()=>{
-       onToggle(task.id) 
-    }}>
-        <h3> 
-            {task.text} 
-            <FaTimes 
-                style={{color: 'red', cursor: 'pointer'}} 
-                onClick = {() => onDelete(task.id)}
-            />
-        </h3>
-        <p style={{color: 'rgb(2, 65, 16'}}> {task.day} </p>
-    </div>
+    <Tooltip
+    title="Doubleclick to toggle reminder"
+    position="bottom"
+    trigger="mouseenter"
+    theme= "dark"
+    effect= "float"
+    >
+      <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={()=>{
+          onToggle(task.id) 
+      }}>
+          <h3> 
+              {task.text} 
+              <FaTimes 
+                  style={{color: 'red', cursor: 'pointer'}} 
+                  onClick = {() => onDelete(task.id)}
+              />
+          </h3>
+          <p style={{color: 'rgb(2, 65, 16'}}> {task.day} </p>
+      </div>
+    </Tooltip>
   )
 }
 
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,18 +1,11 @@
-import { Tooltip } from "react-tippy"
 import Task from "./Task"
 
 const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
-      <Tooltip
-        title="Doubleclick to toggle reminder"
-        position="bottom"
-        trigger="click"
-      >
-        {tasks.map((task) => (
-            <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}/>
-        ))}
-      </Tooltip>
+      {tasks.map((task) => (
+          <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}/>
+      ))}
     </>
   )
 }
